658c32aa2da55cae0d8b627fccb46e5557dde322
woqzxje <quynhzgaming@gmail.com>
2026-06-23 08:09:49 +0700

    feat: configure Vite server settings and backend environment variable

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), basicSsl()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: true,   // Mở trên mạng LAN (để mobile truy cập được)
-    https: true,  // Bật HTTPS (bắt buộc cho WebRTC/camera trên mobile)
   },
 })
